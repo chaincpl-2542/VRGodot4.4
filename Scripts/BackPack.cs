8bcbbfcb4e0c7d843c2425c1962646e5996e3934
chaincpl-2542 <chaincpl2542@gmail.com>
2025-05-25 22:00:23 +0700

    update scene

---
+++ Scripts/BackPack.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class BackPack : Node
+{
+}
+
+public class ItemData
+{
+    public string name;
+    public Texture icon;
+    public PackedScene itemScene;
+}
